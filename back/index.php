563c8173b62e3dce5f7e31b5b74bbee1eb92d9d8
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-02-17 17:53:13 +0100

    création et utilisation d'une fonction php pour créer un header réutilisable

--- back/index.php
+++ back/index.php
@@ -17,20 +17,7 @@
 
     </head>
     <body>
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Orange_logo.svg/1022px-Orange_logo.svg.png" style="width: 30px; margin-right: 10px; margin-left: 10px;" />
-                <span class="fs-4">Simple header</span>
-            </a>
-
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-            </ul>
-        </header>
+        <?= Draw::header() ?>
 
         <div id="ve-components" class="container">
             <?php
--- back/preview/index.php
+++ back/preview/index.php
@@ -21,22 +21,7 @@ cors();
     <body>
         <?php $component = json_decode(file_get_contents('php://input'), true); ?>
         
-        <?php if (empty($component)): ?>
-            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Orange_logo.svg/1022px-Orange_logo.svg.png" style="width: 30px; margin-right: 10px; margin-left: 10px;" />
-                    <span class="fs-4">header</span>
-                </a>
-
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                </ul>
-            </header>
-        <?php endif; ?>
+        <?= empty($component) ? Draw::header() : '' ?>
 
         <main role="main" id="ve-components" class="container">
             <?= Draw::{empty($component) ? 'void' : $component['_name']}($component) ?>
