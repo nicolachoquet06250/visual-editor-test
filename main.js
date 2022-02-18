a81653a3c93b3e6d046a760afb3a645229f4bf31
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-02-18 09:35:55 +0100

    ajout d'un spinner boosted à l'initialisation de l'éditeur visuel et au click sur le bouton "Save"

--- main.js
+++ main.js
@@ -15,3 +15,46 @@ editor.registerComponent(accordion_name, Accordion);
 editor.registerComponent(tariff_cartridge_name, TariffCartridge);
 
 editor.defineElement();
+
+class Spinner extends HTMLElement {
+  get style() {
+    return `
+    <style>
+      bs-spinner,
+      bs-spinner .overlay {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: rgba(255, 255, 255, .3);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 9999;
+        height: 100vh;
+      }
+    </style>
+    `;
+  }
+
+  get template() {
+    return `
+    <div class="overlay">
+      <div class="text-center">
+        <div class="spinner-border spinner-border-lg" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+  
+  constructor() {
+    super();
+
+    this.innerHTML = this.style + this.template;
+  }
+}
+
+customElements.define('bs-spinner', Spinner);
